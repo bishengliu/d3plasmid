7715166e306368b871ae88648577d63bddf0e97c
Bisheng Liu <b.liu@nki.nl>
2016-09-23 16:27:42 +0200

    need to draw seq count around plasmid

--- d3plasmid.js
+++ d3plasmid.js
@@ -13,8 +13,13 @@ function Plasmid(){
     var cSequence = '';
 
     //set the window width
-    var wWidth = 0;
-    var padding = 20;
+    var width = 0;
+    var padding = 10;
+
+    //need to set other global variables
+
+
+
     //read the data 
     this.read = function(json){
         id = json.id;
@@ -28,52 +33,89 @@ function Plasmid(){
         features.sort(sortByProperty('start'));
         enzymes.sort(sortByProperty('cut'));
 
-        //get the window width
-        wWidth = $(id).width() - padding * 2; //padding on each side
+        //get the width for svg
+        width = $(id).width() - padding * 2; //padding on each side
+        width = width < 250 ? 250 : width;
     }
 
 
     //draw
     this.draw = function(){
         //draw empty svg
-        var svg = drawSVG(id);
+        var svg = drawSVG(id, width, padding);
 
         //draw plasmid
-
+        var plasmid = drawPlasmid(svg, id, width, padding, name);
     }
 
     //redraw
     this.redraw = function(){
+        $(id).empty();
+        width = $(id).width() - padding * 2; //padding on each side
+        width = width < 250 ? 250 : width;
         //draw empty svg
-        var svg = drawSVG(id);
+        var svg = drawSVG(id, width, padding);
 
         //draw plasmid
+        var plasmid = drawPlasmid(svg, id, width, padding, name);
     }
+}
 
 
     //draw empty using d3
-    function drawSVG(id) { 
-        var margin = {top: 20, right: 20, bottom: 20, left: 20};
-        var width = +$(id).width() - margin.left - margin.right;
-        var height = +$(id).width() - margin.top - margin.bottom;
-
+    function drawSVG(id, width, padding) { 
         //calculate the width
         var svg = d3.select(id)
                     .append("svg")
-                    .attr("width", width + margin.left + margin.right)
-                    .attr("height", height + margin.top + margin.bottom)
+                    .attr("width", width + padding * 2)
+                    .attr("height", width + padding * 2)
                         .append("g")
-                            .attr("transform", "translate(" + margin.left +"," + margin.top + ")");
+                            .attr("transform", "translate(" + padding +"," + padding + ")");
         return svg;
     }
 
     //draw plasmid
+    function drawPlasmid(svg, id, width, padding, name){
+        //outer: only emzymes
+        var r_enzy = genRatioVal((width + 2*padding), 2500, 30, 100); // will be updated later
+       //inner and over circle: features
+       console.log([width, padding]);
+       var r_plasmid = width/2 - r_enzy;
+       var r_plasmid_padding = genRatioVal((width + 2*padding), 2500, 2, 10);
+       var twoPi = twoPi = 2* Math.PI;
+       var arc = d3.arc()
+                    .startAngle(0)
+                    .endAngle(twoPi)
+                    .innerRadius(r_plasmid - r_plasmid_padding)
+                    .outerRadius(r_plasmid);
 
+       var backbone = svg.append('g').attr("transform", "translate(" + (r_plasmid + r_enzy) + "," + (r_plasmid + r_enzy) + ")");
 
-    //render google color
-    function colores_google(n) {
-        var colores_g = ["#3366cc", "#dc3912", "#ff9900", "#109618", "#990099", "#0099c6", "#dd4477", "#66aa00", "#b82e2e", "#316395", "#994499", "#22aa99", "#aaaa11", "#6633cc", "#e67300", "#8b0707", "#651067", "#329262", "#5574a6", "#3b3eac"];
-        return colores_g[n % colores_g.length];
+            //plasmid label
+            backbone.append("text")
+                    .attr("class", "plasmidName noEvent")
+                    .attr("display", function(){
+                        return (width + 2*padding) <= 502 ? "none" : "inherit";
+                    })
+                    .attr("dy", ".35em")
+                    .attr("text-anchor", "middle")
+                    .style("font-family", "monospace")
+                    .style("font-size", function(){
+                        var fontSize = genRatioVal((width + 2*padding), 2500, 15, 30);
+                        return fontSize +"px";
+                    })
+                    .style("fill", "#636363")
+                    .text(name);
+            //backbone plasmid
+            backbone.append("path")
+                    .attr("stroke", "#6baed6")
+                    .attr("stroke-width", function(){return width > 800 ? 2 : 1})
+                    .attr("fill", "#c6dbef")
+                    .attr("fill-opacity", .5)
+                    .attr("d", arc);
+
+        //draw enzyme
+        
     }
 
     //generate complementary sequence
@@ -97,7 +139,6 @@ function Plasmid(){
         })
         return cSeqArray.join('');
     }
-
 
     //generate sequence that split with a symbol every 10 nt and output the array
     function formatSeq(sequence){
@@ -156,5 +197,18 @@ function Plasmid(){
         return finalArray.join('');
     }
 
+    //render google color
+    function colores_google(n) {
+        var colores_g = ["#3366cc", "#dc3912", "#ff9900", "#109618", "#990099", "#0099c6", "#dd4477", "#66aa00", "#b82e2e", "#316395", "#994499", "#22aa99", "#aaaa11", "#6633cc", "#e67300", "#8b0707", "#651067", "#329262", "#5574a6", "#3b3eac"];
+        return colores_g[n % colores_g.length];
+    }
 
-}
+    //change the value based on the width
+    function genRatioVal(current, max, minValue, maxValue){
+        var output = minValue;
+        var value = current / max * maxValue;
+        output = value > maxValue ? maxValue : value;
+        output = output < minValue ? minValue : output;
+        return output;
+    }
+
